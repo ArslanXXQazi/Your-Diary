44cad26dff1fd9466ff870c79dcf8c717ab725f8
Arslan Qazi <arslanqazi1999@gmail.com>
2025-07-21 15:43:35 +0500

    update

--- lib/src/views/botom_nav_view/diary_view/diary_view.dart
+++ lib/src/views/botom_nav_view/diary_view/diary_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:yourdiary/src/controller/common_widgets/text_feild_widget.dart';
 import 'package:yourdiary/src/controller/common_widgets/text_widget.dart';
 import 'package:yourdiary/src/controller/constant/app_colors/app_color.dart';
 import 'package:yourdiary/src/controller/constant/app_images/app_images.dart';
@@ -10,210 +11,240 @@ class DiaryView extends StatelessWidget {
   Widget build(BuildContext context) {
     final screenWidth = MediaQuery.sizeOf(context).width;
     final screenHeight = MediaQuery.sizeOf(context).height;
+    TextEditingController searchController = TextEditingController();
+
+    // List of subjects
+    final List<Map<String, dynamic>> subjects = [
+      {
+        'name': 'Math',
+        'task': 'Task 1',
+        'imagePath': AppImages.image1,
+        'progress': 1.0, // Set to 100%
+        'onTap': () {
+          print('Math tapped');
+        },
+      },
+      {
+        'name': 'Science',
+        'task': 'Task 2',
+        'imagePath': AppImages.image1,
+        'progress': 0.19,
+        'onTap': () {
+          print('Science tapped');
+        },
+      },
+      {
+        'name': 'English',
+        'task': 'Task 3',
+        'imagePath': AppImages.image1,
+        'progress': 0.75,
+        'onTap': () {
+          print('English tapped');
+        },
+      },
+      {
+        'name': 'Music',
+        'task': 'Task 3',
+        'imagePath': AppImages.image1,
+        'progress': 1.0,
+        'onTap': () {
+          print('Music tapped');
+        },
+      },
+      {
+        'name': 'Chemistry',
+        'task': 'Task 3',
+        'imagePath': AppImages.image1,
+        'progress': 0.1,
+        'onTap': () {
+          print('Music tapped');
+        },
+      },
+
+    ];
+
     return Scaffold(
       backgroundColor: Colors.grey.shade100,
-      body: Column(children: [
-        Container(
-          height: screenHeight*.3,
-          width: double.infinity,
-          color: AppColor.blue,
-          child: Padding(
-            padding:  EdgeInsets.symmetric(horizontal: screenWidth*.02),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: screenHeight * 0.3,
+              width: double.infinity,
+              color: AppColor.blue,
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.02),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                  BlackText(
-                    text: "Hello",
-                    textColor: Colors.white,
-                  ),
-                  SizedBox(height: screenHeight*.01),
-                  BlackText(
-                    text: "Victoria",
-                    textColor: Colors.white,
-                    fontSize: 25,
-                  ),
-                ],),
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(15),
-                  child: Container(
-                    height: screenHeight*.08,
-                    width: screenWidth*.18,
-                    color: Colors.white,
-                  ),
-                )
-              ],),
-              SizedBox(height: screenHeight*.015),
-
-            ],),
-          ),
-        ),
-        SizedBox(height: screenHeight*.02),
-        Padding(
-          padding:  EdgeInsets.symmetric(horizontal: screenWidth*.02),
-          child: Column(children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                BlackText(
-                  text: "Home Work",
-                  fontSize: 18,
-                  fontWeight: FontWeight.w600,
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            BlackText(
+                              text: "Hello",
+                              textColor: Colors.white,
+                            ),
+                            SizedBox(height: screenHeight * 0.01),
+                            BlackText(
+                              text: "Victoria",
+                              textColor: Colors.white,
+                              fontSize: 25,
+                            ),
+                          ],
+                        ),
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(15),
+                          child: Container(
+                            height: screenHeight * 0.08,
+                            width: screenWidth * 0.18,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: screenHeight * 0.015),
+                  ],
                 ),
-                BlackText(
-                  onTap: (){},
-                  text: "See all subjects",
-                  fontSize: 14,
-                  fontWeight: FontWeight.w400,
-                  textColor: AppColor.blue,
-                ),
-              ],),
-            SizedBox(height: screenHeight*.02),
-            Row(children: [
-              Expanded(child: ClipRRect(
-                borderRadius: BorderRadius.circular(15),
-                child: GestureDetector(
-                  onTap: (){},
-                  child: Container(
-                    height: screenHeight*.27,
-                    color: Colors.white,
-                    padding: EdgeInsets.all(screenWidth*.02),
-                    child: Column(children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              BlackText(
-                                text: "Math",
-                                fontWeight: FontWeight.w600,
-                              ),
-                              BlackText(
-                                text: "Task 1",
-                                fontSize: 14,
-                                fontWeight: FontWeight.w600,
-                                textColor: Colors.grey,
-                              ),
-                            ],),
-                          Stack(
-                            children: [
-                              SizedBox(
-                                height: screenHeight * .05, // Responsive height
-                                width: screenWidth * .11,
-                                child: CircularProgressIndicator(
-                                  value: 0.6,
-                                  valueColor: AlwaysStoppedAnimation<Color>(Colors.blue), // Fixed blue color
-                                  backgroundColor: Colors.grey[200],
-                                  strokeWidth: screenWidth * .012,
+              ),
+            ),
+            SizedBox(height: screenHeight * 0.02),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: screenWidth*.02),
+              child: TextFieldWidget(
+                  controller: searchController,
+                  hintText: "Search for anything",
+                  fillColor: Colors.white,
+                  hintColor: Colors.grey,
+                  prefixIcon: Icon(Icons.search,color: AppColor.blue,size: screenWidth*.08,),
+              ),
+            ),
+            SizedBox(height: screenHeight * 0.02),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.02),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      BlackText(
+                        text: "Home Work",
+                        fontSize: 18,
+                        fontWeight: FontWeight.w600,
+                      ),
+                      BlackText(
+                        onTap: () {
+                          print("See all subjects tapped");
+                        },
+                        text: "See all subjects",
+                        fontSize: 14,
+                        fontWeight: FontWeight.w400,
+                        textColor: AppColor.blue,
+                      ),
+                    ],
+                  ),
+                  GridView.builder(
+                    shrinkWrap: true, // Ensures GridView takes only required space
+                    physics: const NeverScrollableScrollPhysics(),
+                    padding: EdgeInsets.only(top: screenHeight * 0.015, bottom: screenHeight * 0.03),
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2, // 2 containers per row
+                      crossAxisSpacing: screenWidth * 0.03, // Spacing between containers
+                      mainAxisSpacing: screenHeight * 0.02,
+                      childAspectRatio: (screenWidth * 0.45) / (screenHeight * 0.27),
+                    ),
+                    itemCount: subjects.length,
+                    itemBuilder: (context, index) {
+                      final subject = subjects[index];
+                      return ClipRRect(
+                        borderRadius: BorderRadius.circular(15),
+                        child: GestureDetector(
+                          onTap: subject['onTap'],
+                          child: Container(
+                            color: Colors.white,
+                            padding: EdgeInsets.all(screenWidth * 0.02),
+                            child: Column(
+                              children: [
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        BlackText(
+                                          text: subject['name'],
+                                          fontWeight: FontWeight.w600,
+                                        ),
+                                        BlackText(
+                                          text: subject['task'],
+                                          fontSize: 14,
+                                          fontWeight: FontWeight.w600,
+                                          textColor: Colors.grey,
+                                        ),
+                                      ],
+                                    ),
+                                    Stack(
+                                      children: [
+                                        SizedBox(
+                                          height: screenHeight * 0.05,
+                                          width: screenWidth * 0.11,
+                                          child: CircularProgressIndicator(
+                                            value: subject['progress'],
+                                            valueColor: AlwaysStoppedAnimation<Color>(Colors.blue),
+                                            backgroundColor: Colors.grey[200],
+                                            strokeWidth: screenWidth * 0.012,
+                                          ),
+                                        ),
+                                        Positioned(
+                                          left: 0,
+                                          right: 0,
+                                          top: 0,
+                                          bottom: 0,
+                                          child: Center(
+                                            child: subject['progress'] == 1.0
+                                                ? Icon(
+                                              Icons.check_circle_rounded,
+                                              size: screenWidth * 0.1,
+                                              color: AppColor.blue,
+                                            )
+                                                : BlackText(
+                                              text: "${(subject['progress'] * 100).toInt()}%",
+                                              fontSize: 12,
+                                              textColor: AppColor.blue,
+                                              fontWeight: FontWeight.w500,
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
                                 ),
-                              ),
-                              Positioned(
-                                left: 0,right: 0,top: 0,bottom: 0,
-                                child: Center(
-                                  child:
-                                  BlackText(
-                                    text: "90%",
-                                    fontSize: 12,
-                                    textColor: AppColor.blue,
-                                    fontWeight: FontWeight.w500,
+                                SizedBox(height: screenHeight * 0.01),
+                                Expanded(
+                                  child: Container(
+                                    height: double.infinity,
+                                    width: double.infinity,
+                                    child: Image(
+                                      image: AssetImage(subject['imagePath']),
+                                      fit: BoxFit.cover,
+                                    ),
                                   ),
-                                  // Icon(Icons.check_circle_rounded,size: screenWidth*.1,color: AppColor.blue,)
                                 ),
-                              )
-                            ],
+                              ],
+                            ),
                           ),
-
-                        ],),
-                      SizedBox(height: screenHeight*.01),
-                      Expanded(
-                        child: Container(
-                          height: double.infinity,
-                          width: double.infinity,
-                          child: Image(image: AssetImage(AppImages.image1),fit: BoxFit.cover,),
                         ),
-                      )
-                    ],),
+                      );
+                    },
                   ),
-                ),
-              )),
-              SizedBox(width: screenWidth*.03),
-              Expanded(child: ClipRRect(
-                borderRadius: BorderRadius.circular(15),
-                child: GestureDetector(
-                  onTap: (){},
-                  child: Container(
-                    height: screenHeight*.27,
-                    color: Colors.white,
-                    padding: EdgeInsets.all(screenWidth*.02),
-                    child: Column(children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              BlackText(
-                                text: "Math",
-                                fontWeight: FontWeight.w600,
-                              ),
-                              BlackText(
-                                text: "Task 1",
-                                fontSize: 14,
-                                fontWeight: FontWeight.w600,
-                                textColor: Colors.grey,
-                              ),
-                            ],),
-                          Stack(
-                            children: [
-                              SizedBox(
-                                height: screenHeight * .05, // Responsive height
-                                width: screenWidth * .11,
-                                child: CircularProgressIndicator(
-                                  value: 0.6,
-                                  valueColor: AlwaysStoppedAnimation<Color>(Colors.blue), // Fixed blue color
-                                  backgroundColor: Colors.grey[200],
-                                  strokeWidth: screenWidth * .012,
-                                ),
-                              ),
-                              Positioned(
-                                left: 0,right: 0,top: 0,bottom: 0,
-                                child: Center(
-                                  child:
-                                  // BlackText(
-                                  //   text: "90%",
-                                  //   fontSize: 12,
-                                  //   textColor: AppColor.blue,
-                                  //   fontWeight: FontWeight.w500,
-                                  // ),
-                                  Icon(Icons.check_circle_rounded,size: screenWidth*.08,color: AppColor.blue,)
-                                ),
-                              )
-                            ],
-                          ),
-
-                        ],),
-                      SizedBox(height: screenHeight*.01),
-                      Expanded(
-                        child: Container(
-                          height: double.infinity,
-                          width: double.infinity,
-                          child: Image(image: AssetImage(AppImages.image1),fit: BoxFit.cover,),
-                        ),
-                      )
-                    ],),
-                  ),
-                ),
-              )),
-            ],)
-          ],),
-        )
-      ],),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
