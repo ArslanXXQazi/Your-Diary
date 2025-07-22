48305412b9c470bf84300696912b536b4a432121
Arslan Qazi <arslanqazi1999@gmail.com>
2025-07-22 09:52:30 +0500

    update

--- lib/src/views/botom_nav_view/settings_nav_view/settings_nav_view.dart
+++ lib/src/views/botom_nav_view/settings_nav_view/settings_nav_view.dart
@@ -3,6 +3,7 @@ import 'package:get/get_core/src/get_main.dart';
 import 'package:get/get_navigation/get_navigation.dart';
 import 'package:yourdiary/src/controller/common_widgets/text_widget.dart';
 import 'package:yourdiary/src/controller/constant/app_colors/app_color.dart';
+import 'package:yourdiary/src/controller/constant/app_images/app_images.dart';
 
 class SettingsNavView extends StatelessWidget {
   const SettingsNavView({super.key});
@@ -12,12 +13,19 @@ class SettingsNavView extends StatelessWidget {
     final screenWidth = MediaQuery.sizeOf(context).width;
     final screenHeight = MediaQuery.sizeOf(context).height;
     return Scaffold(
+      backgroundColor: Colors.grey.shade100,
       body: Column(
         children: [
           Container(
-            height: screenHeight * 0.28,
+            height: screenHeight * 0.35,
             width: double.infinity,
-            color: AppColor.blue,
+            decoration: BoxDecoration(
+              color: AppColor.blue,
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(20),
+                bottomRight: Radius.circular(20),
+              )
+            ),
             child: Padding(
               padding: EdgeInsets.only(left: screenHeight*.02,right:screenHeight*.02,top: screenHeight*.06),
               child: Column(
@@ -36,19 +44,60 @@ class SettingsNavView extends StatelessWidget {
                       Padding(
                         padding: EdgeInsets.only(left: screenWidth*.2),
                         child: BlackText(
-                          text: "Math",
-                          fontSize: 20,
+                          text: "Settings",
+                          fontSize: 18,
                           textColor: Colors.white,
                           fontWeight: FontWeight.w700,
                         ),
                       ),
                     ],),
-                  SizedBox(height: screenHeight*.03),
-                  
+                  SizedBox(height: screenHeight*.02),
+                  CircleAvatar(
+                    radius: screenWidth*.13,
+                    backgroundColor: Colors.white,
+                  ),
+                  SizedBox(height: screenHeight*.02),
+                  BlackText(
+                    text: "Arslan Qazi",
+                    fontSize: 18,
+                    textColor: Colors.white,
+                    fontWeight: FontWeight.w600,
+                  ),
+                  BlackText(
+                    text: "Teacher",
+                    fontSize: 16,
+                    textColor: Colors.white,
+                    fontWeight: FontWeight.w400,
+                  )
+
                 ],
               ),
             ),
           ),
+          SizedBox(height: screenHeight*.02),
+          Padding(
+            padding:  EdgeInsets.symmetric(horizontal: screenWidth*.02),
+            child: Column(children: [
+              GestureDetector(
+                onTap: (){
+                  print("tappppppppppppppppppp");
+                },
+                child: Container(
+                  padding: EdgeInsets.all(screenWidth*.02),
+                  color: Colors.white,
+                  child: Row(children: [
+                    ImageIcon(AssetImage(AppImages.yourSelf),size:screenWidth*.07,color: AppColor.blue,),
+                    SizedBox(width: screenWidth*.02),
+                    BlackText(
+                      text: "Account Settings",
+                    ),
+                    Spacer(),
+                    Icon(Icons.arrow_forward_ios_outlined,color: AppColor.blue,size: screenWidth*.05,)
+                  ],),
+                ),
+              )
+            ],),
+          )
         ],
       ),
     );
